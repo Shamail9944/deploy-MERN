b07fe89db4597008dcfd9fc63aea63ff285ddef5
Shamail Bin Wasif <Shamail130silverhawk@gmail.com>
2023-12-07 03:38:10 +0500

    aass

--- server/index.js
+++ server/index.js
@@ -42,6 +42,13 @@ server.use(session({
 }));
 server.use(passport.authenticate('session'));
 
+
+server.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
 server.use("/auth", AuthRoute);
 server.use("/brands", BrandRoute);
 server.use("/cart", CartRoute);
