a5099c4096e6febca1ede76095848dccfe57ca39
Shamail Bin Wasif <Shamail130silverhawk@gmail.com>
2023-12-07 01:50:00 +0500

    1

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ server.use(express.json());
 server.use(cors(
     {
         origin: "https://deploy-mern-frontend-sable.vercel.app",
-        methods: "GET, POST, PUT, DELETE",
+        methods: "GET, Post, PUT, DELETE",
         credentials: "true",
         exposedHeaders: ["Total-Results"]
     }
