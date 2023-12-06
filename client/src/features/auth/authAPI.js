cfe8735767f6f8823f0d6d2e4c769ec5218f0cf8
Shamail Bin Wasif <Shamail130silverhawk@gmail.com>
2023-12-07 00:09:22 +0500

    1

--- client/src/features/auth/authAPI.js
+++ client/src/features/auth/authAPI.js
@@ -4,7 +4,7 @@ export function createUser(userData) {
     const response = await fetch('https://deploy-mern-server-po2f8lyo0-shamail9944.vercel.app/auth/createUser', {
       method: "POST",
       body: JSON.stringify(userData),
-      headers: { "content-type": "application/json" }
+      headers: { "content-type": "application/json", "Access-Control-Allow-Origin": "*" }
     })
     const data = await response.json()
     resolve({ data })
@@ -17,7 +17,7 @@ export function checkUser(loginData) {
       const response = await fetch('https://deploy-mern-server-po2f8lyo0-shamail9944.vercel.app/auth/checkUser', {
         method: "POST",
         body: JSON.stringify(loginData),
-        headers: { "content-type": "application/json" }
+        headers: { "content-type": "application/json", "Access-Control-Allow-Origin": "*" }
       })
 
       if (response.ok) {
