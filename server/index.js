a4e5bcc1b25c54ec17c27e61481a492a7d047a28
Shamail Bin Wasif <Shamail130silverhawk@gmail.com>
2023-12-07 03:39:12 +0500

    as

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ server.use(passport.authenticate('session'));
 
 server.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
     next();
 });
